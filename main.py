fdbabe29af19efcee379304b5fcdb2bf67abc9e6
efekaanefe <efekaanefe1905@gmail.com>
2023-06-29 21:21:27 +0300

    it beats medium bot

--- main.py
+++ main.py
@@ -14,11 +14,17 @@ SCV = UnitTypeId.SCV
 
 class MyDeadlyTerranBot(BotAI):
     async def on_step(self, iteration):
+
+
         await self.build_workers()
         await self.build_supply_depots()
+        await self.build_refinery()
+        await self.assign_scv_to_refinery()
         await self.build_barracks()
         await self.train_marines()
         await self.attack_enemy()
+
+
 
     async def build_workers(self):
         if self.can_afford(UnitTypeId.SCV) and self.units(UnitTypeId.SCV).amount < 16:
@@ -28,38 +34,61 @@ class MyDeadlyTerranBot(BotAI):
                     self.do(cc.train(UnitTypeId.SCV))
 
     async def build_supply_depots(self):
-        if self.supply_left < 5 and not self.already_pending(UnitTypeId.SUPPLYDEPOT):
-            command_centers = self.townhalls.ready
-            if command_centers.exists:
-                if self.can_afford(UnitTypeId.SUPPLYDEPOT):
-                    await self.build(UnitTypeId.SUPPLYDEPOT, near=command_centers.first)
+        if self.supply_left < 10:
+            if not self.already_pending(UnitTypeId.SUPPLYDEPOT):
+                command_centers = self.townhalls.ready
+                if command_centers.exists:
+                    if self.can_afford(UnitTypeId.SUPPLYDEPOT):
+                        await self.build(UnitTypeId.SUPPLYDEPOT, near=command_centers.first)
+
+
+    async def build_refinery(self):
+        if self.structures(UnitTypeId.REFINERY).amount < 2:
+            if self.can_afford(UnitTypeId.REFINERY):
+                for command_center in self.structures(UnitTypeId.COMMANDCENTER).ready:
+                    vespene_geysers = self.vespene_geyser.closer_than(15, command_center)
+                    for vespene_geyser in vespene_geysers:
+                        if not self.can_afford(UnitTypeId.REFINERY):
+                            break
+                        worker = self.select_build_worker(vespene_geyser.position)
+                        if worker is None:
+                            break
+                        if not self.units(UnitTypeId.REFINERY).closer_than(1, vespene_geyser).exists:
+                            worker.build(UnitTypeId.REFINERY, vespene_geyser)
+
+    async def assign_scv_to_refinery(self):
+        for refinery in self.structures(UnitTypeId.REFINERY).owned.ready:
+            if refinery.assigned_harvesters < refinery.ideal_harvesters:
+                workers = self.units(UnitTypeId.SCV).owned.idle
+                if workers:
+                    worker = workers.closest_to(refinery)
+                    self.do(worker.gather(refinery))
+
 
     async def build_barracks(self):
-        if self.can_afford(UnitTypeId.BARRACKS) and self.units(UnitTypeId.BARRACKS).amount < 3:
-            command_centers = self.townhalls.ready
-            if command_centers.exists:
-                if self.can_afford(UnitTypeId.BARRACKS):
-                    await self.build(UnitTypeId.BARRACKS, near=command_centers.first)
+        if self.structures(UnitTypeId.BARRACKS).amount < 3:
+            if self.can_afford(UnitTypeId.BARRACKS):
+                command_centers = self.townhalls.ready
+                if command_centers.exists:
+                    if self.can_afford(UnitTypeId.BARRACKS) and not self.already_pending(UnitTypeId.BARRACKS):
+                        map_center = self.game_info.map_center
+                        placement_position = self.start_location.towards(map_center, distance=5)
+                        await self.build(UnitTypeId.BARRACKS, near=placement_position)
 
     async def train_marines(self):
-        barracks = self.units(UnitTypeId.BARRACKS).ready
+        barracks = self.structures(UnitTypeId.BARRACKS)
         for barrack in barracks:
             if self.can_afford(UnitTypeId.MARINE) and barrack.is_idle:
                 self.do(barrack.train(UnitTypeId.MARINE))
 
     async def attack_enemy(self):
-        if self.units(UnitTypeId.MARINE).amount >= 10:
+        if self.units(UnitTypeId.MARINE).idle.amount >= 30:
             target = self.enemy_start_locations[0]
             marines = self.units(UnitTypeId.MARINE)
             for marine in marines:
                 self.do(marine.attack(target))
 
 
-
-
-
-
-    
 
 run_game(maps.get("AcropolisLE"), [
     Bot(Race.Terran, MyDeadlyTerranBot()),
